f1797ac84d7df22953f318ee3209bd9b6c3b9583
pajic2017 <pajic.it.2017@gmail.com>
2026-01-08 09:52:12 +0100

    Add logger to main method

--- src/main/java/com/paj/dra_crud_postgres/DraCrudPostgresApplication.java
+++ src/main/java/com/paj/dra_crud_postgres/DraCrudPostgresApplication.java
@@ -1,18 +1,22 @@
 package com.paj.dra_crud_postgres;
 
-import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DraCrudPostgresApplication {
 
-    //Logger log= (Logger) LoggerFactory.getILoggerFactory(DraCrudPostgresApplication.class);
+    //private static final Logger log= (Logger) LoggerFactory.getILoggerFactory(DraCrudPostgresApplication.class.getName());
+
+    private static final java.util.logging.Logger log= Logger.getLogger(DraCrudPostgresApplication.class.getName());
 
     public static void main(String[] args) {
 
         SpringApplication.run(DraCrudPostgresApplication.class, args);
+        log.info("My interaction with console in DRA CRUD POSTGRES example ###");
     }
 
 }
